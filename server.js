39d6ca88f208abd5787a01019caf968fd44fc64a
Erik Olson <oerik100@gmail.com>
2023-03-27 16:45:06 -0400

    connecting to mongodb

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2002
 require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todo'
+
+    MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
