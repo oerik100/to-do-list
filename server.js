a0b7cdeb8a83d92a213a1e47964619ea1e376c2c
Erik Olson <oerik100@gmail.com>
2023-03-27 17:02:15 -0400

    trying to get the port to start displaying the ejs file

--- server.js
+++ server.js
@@ -8,8 +8,23 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-    MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/',(request, response)=>{
+    db.collection('lists').find().sort().toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
